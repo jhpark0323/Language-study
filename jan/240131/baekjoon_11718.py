8f1b9840361af40698dce0dadc9104ed982a7319
jhpark0323 <brianwogus@naver.com>
2024-01-31 23:59:33 +0900

    0131_baekjoon

---
+++ jan/240131/baekjoon_11718.py
@@ -0,0 +1,17 @@
+# try:
+#     while 1:
+#         word = input()
+#         print(word)
+# except:
+#     break
+
+# while 1:
+#     word = input()
+#     print(word)
+
+while 1:
+    try:
+        word = input()
+        print(word)
+    except:
+        break
---
+++ jan/240131/baekjoon_9086.py
@@ -0,0 +1,4 @@
+T = int(input())
+for _ in range(T):
+    word = input()
+    print(word[0] + word[-1])
