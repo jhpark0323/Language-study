6301497b0144d408202a2e893bf408a2cc355d26
jhpark0323 <brianwogus@naver.com>
2024-02-10 16:56:02 +0900

    240210_baekjoon

---
+++ feb/240210/multiples_divisors_5086.py
@@ -0,0 +1,10 @@
+while 1:
+    a, b = map(int, input().split())
+    if a == 0 and b == 0:
+        break
+    elif a % b == 0:
+        print('multiple')
+    elif b % a == 0:
+        print('factor')
+    else:
+        print('neither')
