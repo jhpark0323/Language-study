0b2db9a06436badc993aaba570baf4c67a597ae0
jhpark0323 <brianwogus@naver.com>
2024-03-05 00:32:56 +0900

    외판원다못품

--- march/240304/외판원순회2_10971.py
+++ march/240304/외판원순회2_10971.py
@@ -1,5 +1,26 @@
+from itertools import permutations
+
+'''
+4
+0 10 15 20
+5 0 9 10
+6 13 0 12
+8 8 9 0
+'''
+# 35
+
 n = int(input())
 
 arr = [list(map(int, input().split())) for _ in range(n)]
 print(arr)
 
+# n까지의 순열을 만듬
+p = permutations(range(n))
+answer = float('inf')
+
+for permu in p:
+    # print(i)
+    flag = True
+    temp = 0
+
+    for
