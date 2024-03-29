882a1cdfc2674385d9fe4b0f1ca178e178abdcaf
jhpark <brianwogus@naver.com>
2024-03-29 17:43:19 +0900

    test1

--- march/240329/1 2 3 더하기_9095.py
+++ march/240329/1 2 3 더하기_9095.py
@@ -1,6 +1,18 @@
 T = int(input())
 
+def solve(n):
+  if n == 1:
+    return 1
+  elif n == 2:
+    return 2
+  elif n == 3:
+    return 4
+  else:
+    return solve(n - 1) + solve(n - 2) + solve(n - 3)
+
 for _ in range(T):
     n = int(input())
+    ans = solve(n)
+    print(ans)
 
-    print(n)
+
