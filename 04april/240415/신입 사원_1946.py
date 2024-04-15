9d4d1d52fad655e1b9e2eb52af125a97a32abd7a
jhpark0323 <brianwogus@naver.com>
2024-04-16 00:50:21 +0900

    0416

--- 04april/240415/신입 사원_1946.py
+++ 04april/240415/신입 사원_1946.py
@@ -7,31 +7,14 @@ for _ in range(T):
     arr.sort(reverse=True)
     # print(arr)
 
-    # 1부터 n까지의 모든 등수를 나타내는 ls
-    rank = set(i for i in range(1, n+1))
-    minus = set()
-    for i in range(n):
-        if arr[i][1] == 1:
-            # 1이나온 idx 찾기
-            idx = i
-            break
-        # 1이 아니면
-        else:
-            minus.add(arr[i][1])
-    # print(minus)
+    arr.sort()
 
-    new_ls = list(rank - minus)
-    # print(new_ls)
-
-    cnt = 0
-    new_set_idx = 0
-    ls = []
-    for i in range(idx, n):
-        if arr[i][1] == new_ls[new_set_idx] and arr[i][1] not in ls:
+    cnt = 1
+    max_ = arr[0][1]
+    for i in range(1, n):
+        if max_ > arr[i][1]:
             cnt += 1
-            new_set_idx += 1
-        else:
-            ls.append(arr[i][1])
+            max_ = arr[i][1]
 
     print(cnt)
 
