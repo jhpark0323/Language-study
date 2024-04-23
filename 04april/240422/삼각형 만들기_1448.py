a7a81d67dc6343b59fdcaf03903a377254bb5fca
jhpark <brianwogus@naver.com>
2024-04-23 17:03:10 +0900

    0423

--- 04april/240422/삼각형 만들기_1448.py
+++ 04april/240422/삼각형 만들기_1448.py
@@ -5,4 +5,13 @@ ls = [int(input()) for _ in range(n)]
 # print(ls)
 
 ls.sort(reverse=True)
+# print(ls)
 
+for i in range(n-2):
+    a, b, c = ls[i], ls[i+1], ls[i+2]
+
+    if a < b + c:
+        print(a+b+c)
+        break
+else:
+    print(-1)
