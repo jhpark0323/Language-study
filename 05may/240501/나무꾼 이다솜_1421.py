1b840e411eadd0b87355b3e2f3ccec68656da154
jhpark0323 <brianwogus@naver.com>
2024-05-01 19:35:34 +0900

    0501

---
+++ 05may/240501/나무꾼 이다솜_1421.py
@@ -0,0 +1,5 @@
+import sys
+input = sys.stdin.readline
+n, c, w = map(int, input().split())
+ls = [int(input()) for _ in range(n)]
+
---
+++ 05may/240501/컴백홈_1189.py
@@ -0,0 +1,7 @@
+r, c, k = map(int, input().split())
+arr = [list(input()) for _ in range(r)]
+# print(arr)
+
+di = [1, -1, 0, 0]
+dj = [0, 0, 1, -1]
+
