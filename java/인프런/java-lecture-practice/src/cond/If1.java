f2875cd817bea71268215113ff38001b6f28ea8f
jhpark0323 <brianwogus@naver.com>
2024-06-10 01:07:07 +0900

    인프런강의

---
+++ java/인프런/java-lecture-practice/src/cond/If1.java
@@ -0,0 +1,13 @@
+package cond;
+
+public class If1 {
+    public static void main(String[] args) {
+        int age = 20;
+
+        if (age >= 18) {
+            System.out.println("성인입니다");
+        } else {
+            System.out.println("미성년자입니다.");
+        }
+    }
+}
