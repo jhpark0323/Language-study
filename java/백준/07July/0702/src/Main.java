62f88c7846b5b3da8d17112ea8658d8fe0246693
jhpark <brianwogus@naver.com>
2024-07-02 15:55:41 +0900

    Ultimate

--- java/백준/07July/0702/src/Main.java
+++ java/백준/07July/0702/src/Main.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -16,7 +15,7 @@ public class Main {
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
+        
         int left = 0;
         int right = n - 1;
         int total = Integer.MAX_VALUE;
