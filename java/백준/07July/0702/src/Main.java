8846f505fc723801c9a5d7c95fdd115b6930118b
jhpark <brianwogus@naver.com>
2024-07-02 15:09:07 +0900

    0702_java

--- java/백준/07July/0702/src/Main.java
+++ java/백준/07July/0702/src/Main.java
@@ -1,15 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+
+        int left = 0;
+        int right = n - 1;
+        int total = Integer.MAX_VALUE;
+
+        int[] idx = new int[2];
+
+        while (left < right) {
+            int sum = Math.abs(arr[left] + arr[right]);
+
+            if (total > sum) {
+                total = sum;
+                idx[0] = left;
+                idx[1] = right;
+            }
+
+            if (sum == 0) {
+                idx[0] = left;
+                idx[1] = right;
+                break;
+            } else if (arr[left] + arr[right] < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        System.out.println(arr[idx[0]] + " " + arr[idx[1]]);
+
     }
 }
