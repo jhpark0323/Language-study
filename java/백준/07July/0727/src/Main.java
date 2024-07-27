b829cdb4a2799754b08c7083a069b97c552c047a
jhpark0323 <brianwogus@naver.com>
2024-07-27 23:53:54 +0900

    0727_java

---
+++ java/백준/07July/0727/src/Main.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[n];
+        dp[0] = arr[0];
+        if (n >= 2) {
+            dp[1] = arr[0] + arr[1];
+            if (n >= 3) {
+                dp[2] = Math.max(dp[1], Math.max(dp[0] + arr[2], arr[1] + arr[2]));
+                for (int i = 3; i < n; i++) {
+                    dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]));
+                }
+            }
+        }
+
+
+        System.out.println(dp[n - 1]);
+    }
+}
