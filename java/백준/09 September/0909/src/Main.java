a8f957801ca37b64616d6315554965bf4edcc016
jhpark0323 <brianwogus@naver.com>
2024-09-09 19:57:53 +0900

    0909_java

--- java/백준/09 September/0909/src/Main.java
+++ java/백준/09 September/0909/src/Main.java
@@ -11,12 +11,29 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
+        int[] arr = new int[n+1];
+        for (int i = 1; i <= n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        
+        int[] dp = new int[k + 1];
+
+        for (int i = 1; i <= k; i++) {
+            dp[i] = Integer.MAX_VALUE-1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = arr[i]; j <= k; j++) {
+                dp[j] = Math.min(dp[j], dp[j - arr[i]] + 1);
+//                System.out.println(Arrays.toString(dp));
+            }
+        }
+
+        if (dp[k] == Integer.MAX_VALUE-1) {
+            System.out.println(-1);
+        } else {
+            System.out.println(dp[k]);
+        }
 
     }
 }
