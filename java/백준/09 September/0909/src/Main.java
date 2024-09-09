1a04e9a1a6fea0529ae441e408af9a4d6f4b5246
jhpark <brianwogus@naver.com>
2024-09-09 17:56:20 +0900

    0909_java_ing

---
+++ java/백준/09 September/0909/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+
+// 백준 2994 동전 2
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        
+
+    }
+}
