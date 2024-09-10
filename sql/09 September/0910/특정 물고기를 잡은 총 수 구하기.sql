a276f80aedbc08ab386498c29196263497bd8c93
jhpark <brianwogus@naver.com>
2024-09-10 09:00:29 +0900

    0910_sql

---
+++ sql/09 September/0910/특정 물고기를 잡은 총 수 구하기.sql
@@ -0,0 +1,14 @@
+-- 코드를 작성해주세요
+SELECT
+    COUNT(*) AS FISH_COUNT
+FROM
+    FISH_INFO
+WHERE
+    FISH_TYPE IN (
+    SELECT
+        FISH_TYPE
+    FROM
+        FISH_NAME_INFO
+    WHERE
+        FISH_NAME = "BASS" OR FISH_NAME = "SNAPPER"
+    )
