b8bc293e237d7e446a36f3f3c29fcdf8a96b21e4
jhpark0323 <brianwogus@naver.com>
2024-10-20 22:47:15 +0900

    1020_py

--- python/10 October/1018/페그 솔리테어_9207.py
+++ python/10 October/1018/페그 솔리테어_9207.py
@@ -3,6 +3,13 @@ from pprint import pprint
 input = sys.stdin.readline
 n = int(input())
 
+# def back(cnt):
+#     for i in range(5):
+#         for j in range(9):
+
+
+
+
 for test_case in range(n):
     arr = [list(input().strip()) for _ in range(5)]
     input()
