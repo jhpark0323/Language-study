3796744e4d1a5159720c7997891f19779e56cc12
jhpark0323 <brianwogus@naver.com>
2025-03-07 23:27:16 +0900

    0307

---
+++ java/백준/2025/03 Mar/0308/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        BigInteger[] arr = new BigInteger[10001];
+        arr[0] = BigInteger.ZERO;
+        arr[1] = BigInteger.ONE;
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i-1].add(arr[i-2]);
+        }
+        System.out.println(arr[n]);
+    }
+}
